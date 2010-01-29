4396ee60c84392919ea9c0cb91a19ae63b19c6de
xaicron <xaicron@gmail.com>
2010-01-29 20:23:13 +0900

    initial commit

---
+++ lib/LRU/Cache.pm
@@ -0,0 +1,34 @@
+package LRU::Cache;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+LRU::Cache -
+
+=head1 SYNOPSIS
+
+  use LRU::Cache;
+
+=head1 DESCRIPTION
+
+LRU::Cache is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
